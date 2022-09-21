1f06baec0fc3b838b399c2f9d8f69948473367c2
chezmarcbrown <marc@chezbrown.org>
2022-09-20 16:41:53 -0800

    end of class checkpoint

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello',
+    'tasks',
     'classtoday',
 ]
 
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
-TIME_ZONE = 'America/Anchorage'
+#TIME_ZONE = 'America/Anchorage'
 
 USE_I18N = True
 
